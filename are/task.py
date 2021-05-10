72ca234f98a60d3f6e9ee39534dd1bead6290408
ayziao <ayziao+git@gmail.com>
2021-05-11 06:24:09 +0900

    ♻️ タスク管理 タスクのリポジトリモジュール作る

--- are/task.py
+++ are/task.py
@@ -1,5 +1,6 @@
-from flask import Blueprint, render_template, request, redirect, flash, url_for, abort, make_response  # , g, current_app
-from are.db import get_db
+from flask import Blueprint, render_template, request, redirect, flash, url_for, abort, \
+    make_response  # , g, current_app
+from are.db import get_db, task
 from are.auth import login_required
 
 bp = Blueprint('task', __name__, url_prefix='/x/task')
@@ -12,57 +13,7 @@ def index():
     if change:
         args[change] = request.args.get('to', '')
 
-    where = ' WHERE "状態" <> "特殊な状態"'
-    if args['owner']:
-        if args['owner'][0] == '-':
-            where += ' AND "所有者" <> "' + args['owner'][1:] + '" '
-        else:
-            where += ' AND "所有者" = "' + args['owner'] + '" '
-    if args['rate']:
-        if 'only' in args['rate']:
-            where += ' AND "重要度" = "' + args['rate'][0] + '" '
-        else:
-            where += ' AND "重要度" <= "' + args['rate'] + '"  AND "重要度" <> 0'
-    if args['cost']:
-        where += ' AND "コスト" = "' + args['cost'] + '" '
-    if args['tag']:
-        if args['tag'][0] == '-':
-            where += ' AND "タグ" NOT LIKE "% ' + args['tag'][1:] + ' %" '
-        else:
-            where += ' AND "タグ" LIKE "% ' + args['tag'] + ' %" '
-    if args['cycle']:
-        if args['cycle'] == "routine":
-            where += ' AND ("所有者" = "年" OR "所有者" = "月" OR "所有者" = "週" OR "所有者" = "日" ' \
-                     ' OR "所有者" = "半期" OR "所有者" = "季" OR "所有者" = "寝" OR "所有者" = "食")'
-        elif args['cycle'] == "randomly":
-            where += ' AND ("タグ" LIKE "%繰り返し%" OR "タグ" LIKE "%常備%") '
-        else:
-            where += ' AND "所有者" <> "年" AND "所有者" <> "月" AND "所有者" <> "週" AND "所有者" <> "日" ' \
-                     ' AND "所有者" <> "半期" AND "所有者" <> "季" AND "所有者" <> "寝" AND "所有者" <> "食" ' \
-                     ' AND "タグ" NOT LIKE "%常備%" AND "タグ" NOT LIKE "%繰り返し%" '
-    if args['title']:
-        where += ' AND "タスク名" LIKE "%' + args['title'] + '%" '
-
-    order = ' ORDER BY "状態" DESC, "完了日時" DESC, ' \
-            ' CASE "重要度" WHEN 0 THEN 9 ELSE "重要度" END DESC, ' \
-            ' "変更日時" DESC '
-    if args['sort'] == 'time':
-        order = ' ORDER BY "状態" DESC, "完了日時" DESC, "連番" DESC '
-    if args['sort'] == 'update':
-        order = ' ORDER BY "状態" DESC, "完了日時" DESC, "変更日時" DESC '
-    if args['sort'] == 'cost':
-        order = ' ORDER BY "状態" DESC, "完了日時" DESC, "コスト" '
-    if args['sort'] == 'title':
-        order = ' ORDER BY "状態" DESC, "タスク名" '
-
-    sql = 'SELECT *, ' \
-          ' strftime("%Y-%m-%d ", "完了日時") || ' \
-          '     substr("0"||(strftime("%H", "完了日時")+9),-2,2) || ' \
-          '     strftime(":%M:%S", "完了日時") as utcP9time ' \
-          ' FROM task ' + where + order
-    rows = get_db().execute(sql).fetchall()
-
-    # current_app.logger.debug(sql)
+    rows = task.get_list(args)
 
     joutai = ''
     tags = {}
@@ -357,7 +308,6 @@ def costlist():
         ' ORDER BY コスト DESC '
     ).fetchall()
     return render_template('task/list.html', list=ret, type='コスト')
-
 
 
 @bp.route('/コスト集計', methods=('GET',))
