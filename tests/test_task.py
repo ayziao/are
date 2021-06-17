c5c1992b53bca5091b91e16e1d1760f21ca33a6f
ayziao <ayziao+git@gmail.com>
2021-06-17 15:10:38 +0900

    ✅ タスク管理 絞り込み条件プルダウン化

--- tests/test_task.py
+++ tests/test_task.py
@@ -19,14 +19,14 @@ def test_ログイン状態(client, auth):
 @pytest.mark.parametrize('path, pagetitle', ([
     ('/x/task', '重要度順'),
     ('/x/task?cycle=', '全種'),
-    ('/x/task?cycle=none', '単発'),
-    ('/x/task?cycle=routine', '定期'),
-    ('/x/task?cycle=randomly', '不定'),
+    ('/x/task?cycle=none', 'selected>単発'),
+    ('/x/task?cycle=routine', 'selected>定期'),
+    ('/x/task?cycle=randomly', 'selected>不定'),
     ('/x/task?sort=', '重要度順'),
-    ('/x/task?sort=time', '登録時順'),
-    ('/x/task?sort=update', '更新時順'),
-    ('/x/task?sort=cost', 'コスト順'),
-    ('/x/task?sort=title', 'タスク名順'),
+    ('/x/task?sort=time', 'selected>登録順'),
+    ('/x/task?sort=update', 'selected>更新順'),
+    ('/x/task?sort=cost', 'selected>コスト順'),
+    ('/x/task?sort=title', 'selected>タスク名順'),
 ]))
 def test_タスク絞り込みタイトル(client, path, pagetitle):
     response = client.get(path).data.decode('utf-8')
