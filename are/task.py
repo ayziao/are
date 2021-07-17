da2693d3d2ea820fa3a06627893e629750f721a6
ayziao <ayziao+git@gmail.com>
2021-07-17 14:55:58 +0900

    ✨[タスク管理/アーカイブ]履歴:履歴を見たいので

--- are/task.py
+++ are/task.py
@@ -601,6 +601,144 @@ def archive():
     return redirect(url_for('task.集計'))
 
 
+@bp.route('/archiveupdate', methods=('GET',))
+def archiveupdate():
+    return 'archiveupdate'
+
+
+@bp.route('/archivecostup', methods=('GET',))
+def archivecostup():
+    return 'archivecostup'
+
+
+@bp.route('/history', methods=('GET',))
+def history():
+    args = get_args()
+
+    change = request.args.get('change', '')
+    if change:
+        args[change] = request.args.get('to', '')
+        return redirect(url_for('task.history', **args))
+
+    addtag = request.args.get('addtag', '')
+    if addtag:
+        tagall = args['tag'].split()
+        if addtag not in tagall:
+            tagall.append(addtag)
+            args['tag'] = ' '.join(tagall)
+        return redirect(url_for('task.history', **args))
+
+    deltag = request.args.get('deltag', '')
+    if deltag:
+        tagall = args['tag'].split()
+        tagall.remove(deltag)
+        args['tag'] = ' '.join(tagall)
+        return redirect(url_for('task.history', **args))
+
+    hidetag = request.args.get('hidetag', '')
+    if hidetag:
+        tagall = args['tag'].split()
+        tagall = ['-' + hidetag if tag == hidetag else tag for tag in tagall]
+        args['tag'] = ' '.join(tagall)
+        return redirect(url_for('task.index', **args))
+
+    showtag = request.args.get('showtag', '')
+    if showtag:
+        tagall = args['tag'].split()
+        tagall = [showtag if tag == '-' + showtag else tag for tag in tagall]
+        args['tag'] = ' '.join(tagall)
+        return redirect(url_for('task.history', **args))
+
+    if 'notag' in request.args:
+        args['notag'] = ''
+
+    where = ' WHERE "状態" <> "特殊な状態"'
+
+    if args['status']:
+        if args['status'][0] == '-':
+            where += ' AND "状態" <> "' + args['status'][1:] + '" '
+        else:
+            where += ' AND "状態" = "' + args['status'] + '" '
+    if args['owner']:
+        if args['owner'][0] == '-':
+            where += ' AND "所有者" <> "' + args['owner'][1:] + '" '
+        else:
+            where += ' AND "所有者" = "' + args['owner'] + '" '
+    if args['rate']:
+        if 'only' in args['rate']:
+            where += ' AND "重要度" = "' + args['rate'][0] + '" '
+        elif 'over' in args['rate']:
+            where += ' AND "重要度" >= "' + args['rate'][0] + '" '
+        else:
+            where += ' AND "重要度" <= "' + args['rate'] + '"  AND "重要度" <> 0 '
+    if args['cost']:
+        where += ' AND "予測値" = "' + args['cost'] + '" '
+    if args['tag1st']:
+        if args['tag1st'][0] == '-':
+            where += ' AND "タグ" NOT LIKE " ' + args['tag1st'][1:] + ' %" '
+        else:
+            where += ' AND "タグ" LIKE " ' + args['tag1st'] + ' %" '
+    if args['tag']:
+        tagall = args['tag'].split()
+        for tag in tagall:
+            if tag[0] == '-':
+                where += ' AND "タグ" NOT LIKE "% ' + tag[1:] + ' %" '
+            else:
+                where += ' AND "タグ" LIKE "% ' + tag + ' %" '
+    if 'notag' in args:
+        where += ' AND ("タグ" = "" OR "タグ" = "  ") '
+    if args['cycle']:
+        if args['cycle'] == "routine":
+            where += ' AND ("タグ" LIKE "% 年 %" OR "タグ" LIKE "% 半期 %" OR "タグ" LIKE "%月 %" OR ' \
+                     ' "タグ" LIKE "% 週 %" OR "タグ" LIKE "%日 %" OR "タグ" LIKE "% 季 %" OR ' \
+                     ' "タグ" LIKE "% 春 %" OR "タグ" LIKE "% 夏 %" OR "タグ" LIKE "% 秋 %" OR "タグ" LIKE "% 冬 %")'
+        elif args['cycle'] == "randomly":
+            where += ' AND ("タグ" LIKE "% 繰り返し %" OR "タグ" LIKE "% 常備 %") '
+        else:
+            where += ' AND "タグ" NOT LIKE "% 年 %" AND "タグ" NOT LIKE "% 半期 %" AND "タグ" NOT LIKE "%月 %" ' \
+                     ' AND "タグ" NOT LIKE "% 週 %" AND "タグ" NOT LIKE "%日 %" AND "タグ" NOT LIKE "% 季 %" ' \
+                     ' AND "タグ" NOT LIKE "% 春 %" AND "タグ" NOT LIKE "% 夏 %" AND "タグ" NOT LIKE "% 秋 %" ' \
+                     ' AND "タグ" NOT LIKE "% 冬 %" AND "タグ" NOT LIKE "% 常備 %" AND "タグ" NOT LIKE "% 繰り返し %" '
+    if args['title']:
+        where += ' AND "タスク名" LIKE "%' + args['title'] + '%" '
+
+    order = ' ORDER BY "状態" DESC, "日時" DESC, ' \
+            ' CASE "重要度" WHEN 0 THEN 9 ELSE "重要度" END DESC, ' \
+            ' "日時" DESC '
+    if args['sort'] == 'time':
+        order = ' ORDER BY "状態" DESC, "日時" DESC, "番号" DESC '
+    if args['sort'] == 'update':
+        order = ' ORDER BY "状態" DESC, "日時" DESC '
+    if args['sort'] == 'cost':
+        order = ' ORDER BY "状態" DESC, "日時" DESC, "予測値" '
+    if args['sort'] == 'title':
+        order = ' ORDER BY "状態" DESC, "タスク名" '
+
+    sql = 'SELECT *, 日時 as 完了日時, ' \
+          ' strftime("%Y-%m-%d ", "日時") || ' \
+          '     substr("0"||(strftime("%H", "日時")+9),-2,2) || ' \
+          '     strftime(":%M:%S", "日時") as utcP9time ' \
+          ' FROM task_archive ' + where + order
+
+    print(sql)
+
+    rows = get_db().execute(sql).fetchall()
+
+    joutai = ''
+    tags = {}
+    tasks = {}
+    for item in rows:
+        tags[item['保存番号']] = item['タグ'].split()
+        if item["状態"] == joutai:
+            tasks[item['状態']].append(item)
+        else:
+            joutai = item['状態']
+            tasks[item['状態']] = []
+            tasks[item['状態']].append(item)
+
+    return render_template('task/history.html', tasks=tasks, tags=tags, search=args)
+
+
 @bp.route('/集計', methods=('GET',))
 def 集計():
     args = get_args()
