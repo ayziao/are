3e1f5cd94d72621bc32b72faacb3f11df9821fed
ayziao <ayziao+git@gmail.com>
2021-12-12 10:16:30 +0900

    ♻️[タスク管理]ディレクトリ割り

--- tests/db/test_task.py
+++ tests/db/test_task.py
@@ -1,13 +1,20 @@
 """ タスクリポジトリユニットテスト
 """
-from are.db import task, get_db
+from are.db import get_db
+from are.task import repository
 
 
 def test_create(app):
     with app.app_context():
         db = get_db()
-        task.create(db, 'author', 'owner', 'site', 0, 0, 'title', 'tag', 'body')
+        repository.create(db, 'author', 'owner', 'site', 0, 0, 'title', 'tag', 'body')
         db.commit()
 
         count = db.execute('SELECT COUNT("連番") FROM task').fetchone()[0]
         assert count == 1
+
+def test_第一タグ一覧取得(app):
+    with app.app_context():
+        db = get_db()
+        ret = repository.第一タグ一覧取得()
+        assert ret
